6ccbd22b9fd0a1081317e93d5e672c3a31e3262b
Adam Rudgley <adamrudgley13@gmail.com>
2026-06-05 16:56:38 +1000

    script bug fixes part 2

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const form = document.getElementById('add-form');
-const input = document.getElementById('tasl-input');
+const input = document.getElementById('task-input');
 const list = document.getElementById('task-list');
 
-function addTask(event) {
+function addTask(text) {
     const li = document.createElement('li');
 
     const span = document.createElement('span');
@@ -30,7 +30,7 @@ function addTask(event) {
 form.addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent form from reloading
     const text = input.value.trim();
-    if (text !== '') return;   // do not add empty tasks
+    if (text === '') return;   // do not add empty tasks
     
     addTask(text);
     input.value = ''; // Clear the input field
@@ -53,15 +53,16 @@ function saveTasks() {
 function loadTasks() {
     const saved = localStorage.getItem('tasks');
     if (!saved) return; // No tasks to load
-    JSON.parse(saved).forEach(task){
+    JSON.parse(saved).forEach(function (task) {
         addTask(task.text);
         if (task.done) {
             list.lastChild.classList.add('done');
         }
      });
+   }
      //call saveTasks whenever a task is added, marked done, or deleted
      list.addEventListener('click', saveTasks);
-     list.addEventListener('submit', saveTasks);
+     form.addEventListener('submit', saveTasks);
 
      // Load tasks when the page loads
      loadTasks();
